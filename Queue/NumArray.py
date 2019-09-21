23fd9667b939313dc59cd8a8abab9c3f23b02df2
Casie <abao@math.ubc.ca>
2019-09-20 17:57:07 -0700

    303

---
+++ Queue/NumArray.py
@@ -0,0 +1,28 @@
+# Question 303
+# Idea: 
+# Construct a new array of up-to-point sum, and we can query as many times as we want in the future
+# Time: 98.85%
+# Memory: <=7.95%
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.lst = [0]
+        for i in range(len(nums)):
+            self.lst.append(self.lst[-1]+nums[i])
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+         return self.lst[j+1]-self.lst[i]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
