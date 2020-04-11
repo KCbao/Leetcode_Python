d0312a2d1fff167e935a0237583221349a15b803
an007936 <Casie.Bao@mdacorporation.com>
2020-04-11 10:19:16 -0700

    fix 171

--- Math/171.py
+++ Math/171.py
@@ -1,7 +1,7 @@
 class Solution:
     def titleToNumber(self, s: str) -> int:
         colnum = 0
-        for idx, item in enumerate(s)[::-1]:
+        for i in range(-1, -len(s)-1, -1):
             colnum += (ord(s[i])-64)*26**(-i-1)
         return colnum
         
